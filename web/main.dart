075978b0572b516e6814974392eeba881d4f295b
Plague Fox <plugfox@gmail.com>
2024-06-01 19:23:31 +0400

    Add basoc example

---
+++ web/main.dart
@@ -0,0 +1,69 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:js_interop' as js;
+
+import 'package:web/web.dart' as web;
+
+void main() {
+  const dataKey = 'data';
+  final localStorage = web.window.localStorage;
+
+  final input = Input.getById('dataInput');
+
+  final saveButton = Button.getById('saveButton').addCallback(() {
+    final value = savedValue = input.value;
+    switch (value) {
+      case String text when text.isNotEmpty:
+        localStorage.setItem(dataKey, savedValue = input.value);
+      default:
+        localStorage.removeItem(dataKey);
+    }
+  });
+
+  final loadButton = Button.getById('loadButton').addCallback(() {
+    savedValue = input.value = localStorage.getItem('data') ?? '';
+  });
+
+  final clearButton = Button.getById('clearButton').addCallback(() {
+    clearInput();
+  });
+}
+
+@js.JS()
+external String savedValue;
+
+@js.JS()
+external void clearInput();
+
+extension type Input._(web.Element _input) implements web.EventTarget, js.JSObject {
+  factory Input.getById(String id) {
+    final element = web.document.getElementById(id);
+    if (element == null) throw UnsupportedError('InputElement with id $id not found');
+    return Input._(element);
+  }
+
+  /// The current value of the input field.
+  external String get value;
+
+  /// Sets the value of the input field.
+  external set value(String value);
+}
+
+extension type Button._(web.Element _button) implements web.Element {
+  factory Button.getById(String id) {
+    final element = web.document.getElementById(id);
+    element?.onClick;
+    if (element == null) throw UnsupportedError('ButtonElement with id $id not found');
+    return Button._(element);
+  }
+
+  js.JSFunction addCallback(void Function() callback) {
+    final callbackJS = (web.Event event) {
+      callback();
+    }.toJS;
+    addEventListener('click', callbackJS);
+    return callbackJS;
+  }
+
+  void removeCallback(js.JSFunction callback) => removeEventListener('click', callback);
+}
